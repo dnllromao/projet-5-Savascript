0582ce121b83d125d6d166f6de568fdd3b1727d8
dnllromao <dnllromao@gmail.com>
2017-06-11 00:27:51 +0200

    L'ajax request

--- 11-Ajax/app.js
+++ 11-Ajax/app.js
@@ -7,17 +7,66 @@
 // afficher le plus petit score tip : sort()
 
 let ajax =  new XMLHttpRequest();
-console.log(ajax);
 
-ajax.overrideMimeType("application/json");  
-ajax.open('get', 'data.json');
+ajax.onload = function() { // .onload called when transaction completes sucessfully
+  if (ajax.status >= 200 && ajax.status < 400) {
+    // Success!
+    let json = JSON.parse(ajax.responseText);
+    let scoreded = addScore(json);
+	scoreded = sortByScore(scoreded);
+	let categories = divideByScore(scoreded);
+	let fromBahrain = filterByCountry(scoreded);
+	console.log(scoreded[scoreded.length - 1 ]);
+	console.log(scoreded[0]);
+  } else {
+    // We reached our target server, but it returned an error
+
+  }
+};
+
+ajax.onerror = function() { //  .oneerror called when operation fails to complete due to an error.
+  // There was a connection error of some sort
+};
+
+ajax.open('GET', 'files/data.json');
+ajax.overrideMimeType("application/json");
 ajax.send();
 
-ajax.onreadystatechange = function (truc) {
-	console.log(ajax);
-	let json = JSON.parse(ajax.responseText);
 
-	
-	console.log(json);
+
+function addScore (data) {
+	return data.map(function(el,index,arr) {
+		el.score = Math.round(Math.random() * 1000);
+		return el;
+	});
 }
+
+function sortByScore (data) {
+	return data.sort(function(a,b) {
+		return (a.score > b.score) ? 1 : -1 ;
+	});
+}
+
+function divideByScore (data) {
+	let list = { a : [], b : [], c : [] };
+
+	data.forEach(function(el,index,arr) {
+		if (el.score > 750) {
+			list.a.push(el);
+		} else if (el.score > 500) {
+			list.b.push(el);
+		} else {
+			list.c.push(el);
+		}
+	});
+
+	return list;
+}
+
+function filterByCountry (data) {
+	return data.filter(function(el,index,arr) {
+		return el.country === 'Bahrain';
+	});
+}
+
 

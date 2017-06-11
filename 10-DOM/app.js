8456f7aaa35d380ca4b35f47cc47f6c06a1da407
dnllromao <dnllromao@gmail.com>
2017-06-12 00:11:25 +0200

    Exploring more

--- 10-DOM/app.js
+++ 10-DOM/app.js
@@ -9,12 +9,14 @@
 
 let h4 = document.createElement('h4');
 h4.innerText = 'Je suis une table';
+h4.id = "table-heading";
 
 let p = document.createElement('p');
-p.innerText = 'Ceci es un lorem'	; // can i do it in one line ?
+//p.appendChild()
+p.innerText = 'Ceci es un lorem'; // can i do it in one line ?
 
 let li = document.createElement('li');
-li.innerText = 'List line';
+li.innerHtml = '<a href="">List line</a>';
 
 let th = document.createElement('th');
 th.innerText = 'Table title';
@@ -22,26 +24,43 @@ th.innerText = 'Table title';
 let td = document.createElement('td');
 td.innerText = 'Line title';
 
-var h4Dom = document.querySelector('.container').insertBefore(h4, document.querySelector('table') );
-// did find how to insert after ?
-document.querySelector('.container').insertBefore(p, document.querySelector('table') );
+h4 = document.getElementsByClassName('container')[0].appendChild(h4);
+
+p = h4.parentNode.appendChild(p);
+p.classList.add('bg-fuchsia');
+
+/*
+// create function, it expects 2 values.
+function insertAfter(newElement,targetElement) {
+    // target is what you want it to go after. Look for this elements parent.
+    var parent = targetElement.parentNode;
+
+    // if the parents lastchild is the targetElement...
+    if (parent.lastChild == targetElement) {
+        // add the newElement after the target element.
+        parent.appendChild(newElement);
+    } else {
+        // else the target has siblings, insert the new element between the target and it's next sibling.
+        parent.insertBefore(newElement, targetElement.nextSibling);
+    }
+}
+*/
 
 document.getElementsByTagName('ul')[0].appendChild(li);
-document.getElementsByTagName('ul')[0].append(li);
+//document.getElementsByTagName('ul')[0].append(li);
 // Differences to Node.appendChild():
 // ParentNode.append() allows you to also append DOMString object, whereas Node.appendChild() only accepts Node objects.
 // ParentNode.append() has no return value, whereas Node.appendChild() returns the appended Node object.
 // ParentNode.append() can append several nodes and strings, whereas Node.appendChild() can only append one node.
 
-document.getElementsByTagName('tr')[0].appendChild(th);
-console.log(document.getElementsByTagName('tr')[0].nextElementSibling);
-document.getElementsByTagName('tr')[0].nextElementSibling.appendChild(td);
+th = document.querySelector('tr').appendChild(th);;
+td = document.getElementsByTagName('tr')[1].appendChild(td);
 
 document.getElementById('first-paragraph').classList.remove('bg-lime');
-let paragh = document.querySelectorAll('.container > p');
-console.log(paragh);
 
-paragh.forEach(function(el, key) {
+let paragphs = document.querySelectorAll('.container > p');
+
+paraghs.forEach(function(el, key) {
 	console.log(el);
 	el.classList.add('shadow');
 });
